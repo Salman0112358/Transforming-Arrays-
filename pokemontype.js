e73945473cb267e75eff0737a9df21d828780c14
Salman H <Salman0112358@outlook.com>
2022-07-04 19:01:05 +0100

    writing out function code

--- pokemontype.js
+++ pokemontype.js
@@ -2,3 +2,35 @@
 [{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]
 -> ["Bulbasaur is a grass type Pokémon.", "Charmander is a fire type Pokémon.", "Squirtle is a water type Pokémon." 
 */
+/*
+function  name: pokemonType
+parameters:
+    - info: array contraining object
+return: string[]
+
+*/
+
+/**
+ * 
+ * @param {anything[]} Pokemon 
+ * @returns - array containing string descript of pokemons 
+ */
+
+function pokemonType(Pokemon){
+
+    const pokemonDescription = []
+    let speciesDescription = ""
+
+    for (let species of Pokemon){
+
+        speciesDescription = `${species.name} is a ${species.type} Pokémon.`
+
+        pokemonDescription.push(speciesDescription)
+
+    }
+
+    return PokemonDescription
+
+}
+
+pokemonType([{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }])
