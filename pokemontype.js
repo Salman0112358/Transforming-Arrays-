55438d064b0365dd7a462d485ad1eb50bcd3a8a9
Salman H <Salman0112358@outlook.com>
2022-07-04 18:51:49 +0100

    example case for pokemon type array map problem

---
+++ pokemontype.js
@@ -0,0 +1,4 @@
+/*
+[{ name: 'Bulbasaur', type: 'Grass' }, { name: 'Charmander', type: 'Fire' }, { name: 'Squirtle', type: 'Water' }]
+-> ["Bulbasaur is a grass type Pokémon.", "Charmander is a fire type Pokémon.", "Squirtle is a water type Pokémon." 
+*/
