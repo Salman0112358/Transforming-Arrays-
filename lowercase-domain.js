a8544c7eadcb36f1e2b2648f9d1dac26c0f34750
Salman H <Salman0112358@outlook.com>
2022-07-04 18:33:49 +0100

    adding pseudo code and function signature

---
+++ lowercase-domain.js
@@ -0,0 +1,17 @@
+
+// function name: lowercaseDomain
+// parameters:
+//  - emailList: array of strings
+// returns: array of strings
+
+
+/**
+ * 
+ * @param {string[]} emailList 
+ * @returns {string[]} 
+ */
+function lowerCaseDomain(emailList){
+
+    return lowercaseList
+
+}
