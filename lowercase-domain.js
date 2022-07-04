1038e73544636c110eb2c1b32cb59192c5e4d507
Salman H <Salman0112358@outlook.com>
2022-07-04 18:34:49 +0100

    adding test cases

--- lowercase-domain.js
+++ lowercase-domain.js
@@ -15,3 +15,15 @@ function lowerCaseDomain(emailList){
     return lowercaseList
 
 }
+
+// test cases
+
+console.log(
+	lowercaseDomains(["academy.tech", "GITHUB.COM", "StackOverflow.com", "mOzIlLa.OrG"]),
+	["academy.tech", "github.com", "stackoverflow.com", "mozilla.org"]
+)
+
+console.log(
+	lowercaseDomains(["m1a.tech", "example.cO.UK", "HTTPS://JavaScript.Info"]),
+	["m1a.tech", "example.co.uk", "https://javascript.info"]
+)
